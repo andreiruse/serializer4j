ad13ca0b350351078917544038715e81a5ccf1fe
Andrei Ruse <andrei@opengamma.com>
2019-02-13 19:17:36 +0000

    Adding custom logic to handle custom defined types

--- src/main/java/Serializer.java
+++ src/main/java/Serializer.java
@@ -1,9 +1,16 @@
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import custom.SerializableType;
+import custom.StringType;
+
 public class Serializer {
+
+  private static final List<SerializableType> CUSTOM_SERIALIZERS = Arrays.asList(new StringType());
 
   public static String serialize(Object o, int levels) throws IllegalAccessException {
     return serialize(o, levels, 0).toString();
@@ -11,6 +18,10 @@ public class Serializer {
 
   public static StringBuilder serialize(Object o, int levels, int indentation) throws IllegalAccessException {
     StringBuilder stringBuilder = new StringBuilder();
+    if (CUSTOM_SERIALIZERS.stream().anyMatch(ser -> ser.isType(o))) {
+      stringBuilder.append(CUSTOM_SERIALIZERS.stream().filter(ser -> ser.isType(o)).map(ser -> ser.serialize(o)).findFirst().get());
+      return stringBuilder;
+    }
     for (Field field : o.getClass().getDeclaredFields()) {
       if (Modifier.isStatic(field.getModifiers())) {
         continue;
@@ -22,6 +33,10 @@ public class Serializer {
           .append(prefix(indentation))
           .append(field.getName())
           .append("=");
+      if (CUSTOM_SERIALIZERS.stream().anyMatch(ser -> ser.isType(o2))) {
+        stringBuilder.append(CUSTOM_SERIALIZERS.stream().filter(ser -> ser.isType(o2)).map(ser -> ser.serialize(o2)).findFirst().get());
+        continue;
+      }
       if (levels > 0 && o2 != null && !field.getType().isPrimitive() && o2.getClass().getDeclaredFields().length > 0) {
         stringBuilder.append(serialize(o2, levels - 1, indentation + 1));
       } else if (o2 == null) {
---
+++ src/test/java/SerializerTest.java
@@ -0,0 +1,30 @@
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.Test;
+
+/**
+ * Tests {@link Serializer}
+ */
+public class SerializerTest {
+
+  @Test
+  public void testSerialize() throws IllegalAccessException {
+    String test = "sample text";
+    String serialized = Serializer.serialize(test, 2);
+    assertEquals(serialized, "sample text");
+  }
+
+  @Test
+  public void testSerializeWith0Indentation() throws IllegalAccessException {
+    String test = "sample text";
+    String serialized = Serializer.serialize(test, 2, 0).toString();
+    assertEquals(serialized, "sample text");
+  }
+
+  @Test
+  public void testSerializeWithExtraIndentation() throws IllegalAccessException {
+    String test = "sample text";
+    String serialized = Serializer.serialize(test, 2, 3).toString();
+    assertEquals(serialized, "sample text");
+  }
+}
